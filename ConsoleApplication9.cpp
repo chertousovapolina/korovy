753c35ca6ee60a572a2487dc6b69621734f5323c
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-06 16:24:02 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,27 @@
+﻿// Чертоусова дз Коровы
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << n << " "<<"korova";
+    }
+    
+    if (n==2 ) {
+        cout << n <<" "<< "korovy";
+    }
+    if (n == 3) {
+        cout << n << " " << "korovy";
+    }
+    if (n == 4) {
+        cout << n << " " << "korovy";
+    }
+    if (n>=5 ) {
+        cout << n << " "<< "korov";
+    }
+}
+
+
